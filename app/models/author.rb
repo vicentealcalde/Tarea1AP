1162ba070b4e162bc4937343ef8474a74aea5c1e
Valcalde <valcalde@miuandes.cl>
2023-08-03 16:11:14 -0400

    add index authors with number of books, average and sales

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,17 @@
 class Author < ApplicationRecord
     has_many :books, dependent: :destroy
+    def number_of_published_books
+        books.count
+      end
+    
+    def average_score
+        total_reviews = books.joins(:reviews).pluck(:score)
+        return 0 if total_reviews.empty?
+
+        total_reviews.sum.to_f / total_reviews.size
+    end
+
+    def total_sales
+        books.joins(:sales).sum(:number_of_sales)
+    end
 end
